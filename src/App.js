f90e6748fdfb604babdd1ca411bf83e6df22ecfe
Owen Gray <oweng@hostkru.com>
2022-09-20 00:29:06 -0500

    silenced warning

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
+// eslint-disable-next-line no-unused-vars
 import firebase from "./FirebaseConfig";
 
 function App() {
+	// console.log(firebase);
 	return (
 		<div className="App">
 			<div className="title-row">
